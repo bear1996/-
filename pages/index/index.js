57a6a398b77f8f5322f470bc72fd8d5b399f0d09
unknown <unknown@example.com>
2018-11-20 20:39:35 +0800

    Initial Commit

---
+++ pages/index/index.js
@@ -0,0 +1,230 @@
+var app = getApp();
+var util = require('../../utils/util.js');
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    location: '',
+    county: '',
+    sliderList: [
+      { selected: true, imageSource: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1542721467277&di=0e6b7bff31d3f17d0a3e0ee227305112&imgtype=0&src=http%3A%2F%2Fimgsrc.baidu.com%2Fimage%2Fc0%253Dshijue1%252C0%252C0%252C294%252C40%2Fsign%3D71ba652c9d0a304e462fa8b9b9a1cdf3%2F241f95cad1c8a78645b25b266d09c93d70cf5079.jpg' },
+      { selected: false, imageSource: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1542721519444&di=1b3406e867150b67e5070a89741fd9ee&imgtype=0&src=http%3A%2F%2Fn.sinaimg.cn%2Fsinacn21%2F79%2Fw550h329%2F20181017%2F365b-hmivixn1580510.jpg' },
+      { selected: false, imageSource: ' https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1542722032864&di=ecbef2df3819f6efd0a024c75ebe833e&imgtype=0&src=http%3A%2F%2Fimgsrc.baidu.com%2Fimage%2Fc0%253Dshijue1%252C0%252C0%252C294%252C40%2Fsign%3D40e62ae888025aafc73f76889384c111%2Fa50f4bfbfbedab6492ba21e8fd36afc378311ec9.jpg' },
+      { selected: false, imageSource: 'https://wx1.sinaimg.cn/mw690/005wsg2wly1fwae2iy09dj32ao328qv7.jpg' },
+    ],
+    today: "",
+    inTheaters: {},
+    containerShow: true,
+    weatherData: '',
+    air: '',
+    dress: ''
+  },
+
+  onLoad: function (options) {
+    //更新当前日期
+    app.globalData.day = util.formatTime(new Date()).split(' ')[0];
+    this.setData({
+      today: app.globalData.day
+    });
+    //定位当前城市
+    this.getLocation();
+    //获取豆瓣电影正在热映信息
+    var inTheatersUrl = app.globalData.doubanBase +
+      "/v2/movie/in_theaters" + "?start=0&count=6";
+    this.getMovieListData(inTheatersUrl, "inTheaters", "正在热映");
+
+    //获取用户信息
+    wx.getUserInfo({
+      success: function (res) {
+        var log = Date.now();
+        res.userInfo.logtime = util.formatTime(new Date(log));
+        var userInfos = wx.getStorageSync('userInfos') || [];
+        userInfos.unshift(res.userInfo);
+        wx.setStorageSync('userInfos', userInfos);
+      }
+    })
+
+  },
+
+  //调用豆瓣api
+  getMovieListData: function (url, settedKey, categoryTitle) {
+    wx.showNavigationBarLoading()
+    var that = this;
+    wx.request({
+      url: url,
+      method: 'GET',
+      header: {
+        "Content-Type": "json"
+      },
+      success: function (res) {
+        that.processDoubanData(res.data, settedKey, categoryTitle)
+      },
+      fail: function (error) {
+        console.log(error)
+      }
+    })
+  },
+  //获得电影数据后的处理方法
+  processDoubanData: function (moviesDouban, settedKey, categoryTitle) {
+    var movies = [];
+    for (var idx in moviesDouban.subjects) {
+      var subject = moviesDouban.subjects[idx];
+      var title = subject.title;
+      if (title.length >= 6) {
+        title = title.substring(0, 6) + "...";
+      }
+      var temp = {
+        stars: util.convertToStarsArray(subject.rating.stars),
+        title: title,
+        average: subject.rating.average,
+        coverageUrl: subject.images.large,
+        movieId: subject.id
+      }
+      movies.push(temp)
+    }
+    var readyData = {};
+    readyData[settedKey] = {
+      categoryTitle: categoryTitle,
+      movies: movies
+    }
+    this.setData(readyData);
+    wx.hideNavigationBarLoading();
+  },
+
+  //定位当前城市
+  getLocation: function () {
+    var that = this;
+    wx.getLocation({
+      type: 'wgs84',
+      success: function (res) {
+        //当前的经度和纬度
+        let latitude = res.latitude
+        let longitude = res.longitude
+        wx.request({
+          url: `https://apis.map.qq.com/ws/geocoder/v1/?location=${latitude},${longitude}&key=${app.globalData.tencentMapKey}`,
+          success: res => {
+            app.globalData.defaultCity = app.globalData.defaultCity ? app.globalData.defaultCity : res.data.result.ad_info.city;
+            app.globalData.defaultCounty = app.globalData.defaultCounty ? app.globalData.defaultCounty : res.data.result.ad_info.district;
+            that.setData({
+              location: app.globalData.defaultCity,
+              county: app.globalData.defaultCounty
+            });
+            that.getWeather();
+            that.getAir();
+          }
+        })
+      }
+    })
+  },
+
+  //引入了电影模板，绑定了点击方法，这里写跳转方法即可
+  onMovieTap: function (event) {
+    var movieId = event.currentTarget.dataset.movieid;
+    wx.navigateTo({
+      url: "../movies/movie-detail/movie-detail?id=" + movieId
+    })
+  },
+  //点击更多电影，跳转页面
+  onMoreTap: function (event) {
+    wx.switchTab({
+      url: '../movies/movies'
+    });
+  },
+
+  //获取天气
+  getWeather: function (e) {
+    var length = this.data.location.length;
+    var city = this.data.location.slice(0, length - 1); //分割字符串
+    console.log(city);
+    var that = this;
+    var param = {
+      key: app.globalData.heWeatherKey,
+      location: city
+    };
+    //发出请求
+    wx.request({
+      url: app.globalData.heWeatherBase + "/s6/weather",
+      data: param,
+      header: {
+        'content-type': 'application/json'
+      },
+      success: function (res) {
+        app.globalData.weatherData = res.data.HeWeather6[0].status == "unknown city" ? "" : res.data.HeWeather6[0];
+        var weatherData = app.globalData.weatherData ? app.globalData.weatherData.now : "暂无该城市天气信息";
+        var dress = app.globalData.weatherData ? res.data.HeWeather6[0].lifestyle[1] : { txt: "暂无该城市天气信息" };
+        that.setData({
+          weatherData: weatherData, //今天天气情况数组 
+          dress: dress //生活指数
+        });
+      }
+    })
+  },
+  //获取当前空气质量情况
+  getAir: function (e) {
+    var length = this.data.location.length;
+    var city = this.data.location.slice(0, length - 1);
+    var that = this;
+    var param = {
+      key: app.globalData.heWeatherKey,
+      location: city
+    };
+    wx.request({
+      url: app.globalData.heWeatherBase + "/s6/air/now",
+      data: param,
+      header: {
+        'content-type': 'application/json'
+      },
+      success: function (res) {
+        app.globalData.air = res.data.HeWeather6[0].status == "unknown city" ? "" : res.data.HeWeather6[0].air_now_city;
+        that.setData({
+          air: app.globalData.air
+        });
+      }
+    })
+  },
+
+  //点击更改定位切换到城市页面
+  jump: function () {
+    //关闭本页去切换城市，返回时就可以重新初始化定位信息哦
+    wx.reLaunch({
+      url: '../switchcity/switchcity'
+    });
+  },
+
+  //点击天气跳转到天气页面
+  gotoWeather: function () {
+    wx.navigateTo({
+      url: '../weather/weather'
+    });
+  },
+
+  //轮播图绑定change事件，修改图标的属性是否被选中
+  switchTab: function (e) {
+    var sliderList = this.data.sliderList;
+    var i, item;
+    for (i = 0; item = sliderList[i]; ++i) {
+      item.selected = e.detail.current == i;
+    }
+    this.setData({
+      sliderList: sliderList
+    });
+  },
+
+  // 用户点击右上角分享
+  onShareAppMessage: function () {
+    return {
+      title: 'e 生活',
+      desc: '分享个小程序，希望你喜欢☺️~',
+      success: function (res) {
+        // 转发成功
+        wx.showToast({
+          title: "分享成功",
+          duration: 1000,
+          icon: "success"
+        })
+      }
+    }
+  }
+})
